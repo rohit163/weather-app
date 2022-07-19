fabb77100d33d62ad47aa5526e2e973de4895e53
Rohit Phatarperkar <54476479+rohit163@users.noreply.github.com>
2022-07-19 15:45:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import "./App.css";
+import Inputs from "./components/Inputs";
+import TimeAndLocation from "./components/TimeAndLocation";
+import TemperatureAndDetails from "./components/TemperatureAndDetails";
+import Forecast from "./components/Forecast";
+import getFormattedWeatherData from "./services/weatherService";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [query, setQuery] = useState({ q: "New York" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        setWeather(data);
+      });
+    };
+
+    fetchWeather();
+  }, [query, units]);
+
+  const gradientBackground = () => {
+    return "from-yellow-700 to-orange-700";
+  };
+
+  return (
+    <div
+      className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400 ${gradientBackground()}`}
+    >
+      <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
+
+      {weather && (
+        <div>
+          <TimeAndLocation weather={weather} />
+          <TemperatureAndDetails weather={weather} />
+
+          <Forecast title="hourly forecast" items={weather.hourly} />
+          <Forecast title="daily forecast" items={weather.daily} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
